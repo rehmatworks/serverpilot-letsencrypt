85ab721b5584b1f87576d854caf97791e7b3bfc4
Rehmat Alam <contact@rehmat.works>
2017-04-30 00:24:42 +0500

    Update sple.sh

--- sple.sh
+++ sple.sh
@@ -138,11 +138,7 @@ elif [ "$theAction" == "install" ]; then
 		echo -e "\e[32mSSL should have been installed for $domainName with auto-renewal (via cron)\e[39m"
 
 		# Add a cron job for auto-ssl renewal
-		if [ "$domainType" == "main" ]; then
-			grep "sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName -d www.$domainName && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName -d www.$domainName && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
-		else
-			grep "sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
-		fi
+		grep "sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
 	elif [[ "$output" == *"Failed authorization procedure."* ]]; then
 		echo -e "\e[31m$domainName isn't being resolved to this server. Please check and update the DNS settings if necessary and try again when domain name points to this server\e[39m"
 	elif [[ ! $output ]]; then
@@ -176,11 +172,7 @@ elif [ "$theAction" == "install" ]; then
 	include /etc/nginx-sp/vhosts.d/$appName.d/*.conf;
 }" > "$spSSLDir$appName-ssl.conf"
 	echo -e "\e[32mSSL should have been installed for $domainName with auto-renewal (via cron)\e[39m"
-		if [ "$domainType" == "main" ]; then
-			grep "sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName -d www.$domainName && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName -d www.$domainName && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
-		else
-			grep "sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt certonly -d $domainName && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
-		fi
+		grep "sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
 	else
 		echo -e "\e[31mSomething unexpected occurred\e[39m"
 	fi 
