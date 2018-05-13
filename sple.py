aae1fae0864bc0de3ccd2a2c25bd865de4b26c7a
Rehmat Alam <contact@rehmat.works>
2018-05-14 00:45:27 +0500

    Updating python package

--- sple.py
+++ sple.py
@@ -118,6 +118,8 @@ def get_ssl(app):
 		domains = app.get('domains')
 		cmd = certbot_command(app.get('root'), domains)
 		cboutput = commands.getstatusoutput(cmd)[1]
+		print(cboutput)
+		return False
 		if 'Congratulations' in cboutput:
 			print(bcolors.OKGREEN+'SSL has been successfully obtained for '+' '.join(domains)+bcolors.ENDC)
 			return True
