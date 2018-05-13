96bbbf74964b0e27d4e573e5035ffb390473ef63
Rehmat Alam <contact@rehmat.works>
2018-05-14 00:14:32 +0500

    Bug fixing

--- sple.py
+++ sple.py
@@ -32,8 +32,9 @@ def find_between(s, first, last):
 		return None
 
 def search(value, data):
-	for blocks in data:
-		# blocks = conf.get('server')
+	for conf in data:
+		print(conf)
+		blocks = conf.get('server')
 		for block in blocks:
 			found = block.get(value)
 			if found:
