38516c66d797d6064e6309f6030ea75449191c7e
Rehmat Alam <contact@rehmat.works>
2018-05-14 20:28:28 +0500

    Updating version for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rwssl',
-	version='3.3',
+	version='3.4',
 	description='ServerPilot Let\'s Encrypt SSL installation automation script.',
 	author="Rehmat",
 	author_email="contact@rehmat.works",
