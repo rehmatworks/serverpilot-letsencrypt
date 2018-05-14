e3d7b19d7c3dc97a606f798e6ec964478b4db603
Rehmat Alam <contact@rehmat.works>
2018-05-14 20:38:50 +0500

    Restarting versioning number from 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rwssl',
-	version='4.0.0',
+	version='1.0.0',
 	description='ServerPilot Let\'s Encrypt SSL installation automation script.',
 	author="Rehmat",
 	author_email="contact@rehmat.works",
