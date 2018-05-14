e8ecefd6b9d05eb7ff5e00e81cd433c754705dee
Rehmat Alam <contact@rehmat.works>
2018-05-14 05:01:06 +0500

    Packaging as a PIP package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='rwssl',
+	version='3.0',
+	description='ServerPilot Let\'s Encrypt SSL installation automation script.',
+	author="Rehmat",
+	author_email="contact@rehmat.works",
+	url="https://github.com/rehmatworks/serverpilot-letsencrypt",
+	license="MIT",
+	entry_points={
+		'console_scripts': [
+			'rwssl = rwssl.rwssl:main',
+		],
+	},
+	packages=[
+		'rwssl',
+	],
+	install_requires=[
+		'python-nginx'
+	],
+)
