f5e55eac4ff8a2f8b771b417765a25fe41e5d43b
Rehmat Alam <contact@rehmat.works>
2018-05-18 15:45:39 +0500

    Testing wild flag

--- rwssl/rwssl.py
+++ rwssl/rwssl.py
@@ -61,15 +61,15 @@ def apps():
 		print(bcolors.FAIL+'No apps found. Ensure that you have created some apps already.'+bcolors.ENDC)
 	return spapps
 
-def certbot_command(root, domains, wildcard):
+def certbot_command(root, domains, wild):
 	domainsstr = ''
 	for domain in domains:
 		domainsstr += ' -d '+domain
-	if wildcard:
-		print('wildcard')
+	if wild:
+		print('Wild')
 		sys.exit()
 	else:
-		print('Non wildcard')
+		print('Non wild')
 		sys.exit()
 	cmd = "certbot certonly --webroot -w "+root+" --register-unsafely-without-email --agree-tos --force-renewal"+domainsstr
 	return cmd
@@ -167,7 +167,7 @@ def renew_ssls():
 	commands.getstatusoutput(cmd)
 	print(bcolors.OKBLUE+'Renewals should have been succeeded for all expiring SSLs.'+bcolors.ENDC)
 
-def get_ssl(app, wildcard):
+def get_ssl(app, wild):
 	print(bcolors.OKBLUE+'Obtaining SSL certificate for the app '+bcolors.BOLD+app.get('appname')+'.'+bcolors.ENDC)
 	checkcertbot = commands.getstatusoutput('certbot')
 	errcodes = [32512]
@@ -179,7 +179,7 @@ def get_ssl(app, wildcard):
 		print(bcolors.OKBLUE+'Retrying SSL certificate retrieval for the app '+bcolors.BOLD+app.get('appname')+'.'+bcolors.ENDC)
 	if(os.path.isdir(app.get('root'))):
 		domains = app.get('domains')
-		cmd = certbot_command(app.get('root'), domains, wildcard)
+		cmd = certbot_command(app.get('root'), domains, wild)
 		cboutput = commands.getstatusoutput(cmd)[1]
 		if 'Congratulations' in cboutput:
 			print(bcolors.OKGREEN+'SSL certificate has been successfully obtained for '+' '.join(domains)+bcolors.ENDC)
@@ -209,8 +209,8 @@ def ssl_status():
 		theapps = {'ssl': sslapps, 'nonssl': nonssl}
 	return theapps
 
-def do_final_ssl_install(app, wildcard):
-	install = get_ssl(app, wildcard)
+def do_final_ssl_install(app, wild):
+	install = get_ssl(app, wild)
 	if(install):
 		write_conf(app)
 
@@ -249,7 +249,7 @@ def refresh_ssl_apps():
 		if(len(sslapps) > 0):
 			print(bcolors.OKBLUE+'Refreshing SSL certificates for '+str(len(sslapps))+' apps. Obsolete vhosts will be cleaned.'+bcolors.ENDC)
 			for app in sslapps:
-				do_final_ssl_install(app, wildcard)
+				do_final_ssl_install(app, wild)
 		else:
 			print(bcolors.FAIL+'No apps found to be refreshed'+bcolors.ENDC)
 
@@ -323,10 +323,10 @@ def main():
 	ap.add_argument('-re', '--refresh', dest='refresh', help='Cleans all previous SSL vhost files, reinstalls the SSLs and reloads nginx. Only needed if you are having issues on a server with old SSL installations.', action='store_const', const=True, default=False)
 	ap.add_argument('-redir', '--redirect', dest='redirect', help='Apply a 301 redirect from HTTP to HTTPs for a given app or for all apps.', default=False)
 	ap.add_argument('-noredir', '--noredirect', dest='noredirect', help='Disable HTTP to HTTPs redirect for a single app or for all apps.', default=False)
-	ap.add_argument('-w', '--wild', dest='wildcardcard', help='Obtain a wildcardcard SSL certificate. DNS challenge is required.', action='store_const', const=True, default=False)
+	ap.add_argument('-w', '--wild', dest='wildcard', help='Obtain a wildcard SSL certificate. DNS challenge is required.', action='store_const', const=True, default=False)
 
 	args = ap.parse_args()
-
+	wildcard = args.wildcard
 	if(not os.path.isdir(vhostsdir)):
 		print(bcolors.FAIL+'This package is intended to be used only on ServerPilot servers. Aborting!'+bcolors.ENDC)
 		sys.exit()
@@ -334,13 +334,13 @@ def main():
 	if args.all is True:
 		apps = apps()
 		for app in apps:
-			do_final_ssl_install(app, wildcardcard)
+			do_final_ssl_install(app, wildcard)
 
 	elif args.appname:
 		vhostfile = get_app_vhost(args.appname)
 		app = get_app_info(vhostfile)
 		if app:
-			do_final_ssl_install(app, wildcard)
+			do_final_ssl_install(app, wild)
 		else:
 			print(bcolors.FAIL+'Provided app name seems to be invalid as we did not find any vhost files for it.'+bcolors.ENDC)
 	elif args.ignoreapps:
@@ -349,7 +349,7 @@ def main():
 		print(bcolors.OKBLUE+str(len(ignoreapps))+' apps are being ignored.'+bcolors.ENDC)
 		for app in apps:
 			if app.get('appname') not in ignoreapps:
-				do_final_ssl_install(app, wildcard)
+				do_final_ssl_install(app, wild)
 	elif args.renew is True:
 		renew_ssls()
 	elif args.installcron is True:
@@ -362,7 +362,7 @@ def main():
 		if(len(nonsslapps) > 0):
 			print(bcolors.OKBLUE+str(len(nonsslapps))+' non-ssl apps found for which SSL can be obtained. Proceeding...'+bcolors.ENDC)
 			for nonssl in nonsslapps:
-				do_final_ssl_install(nonssl, wildcard)
+				do_final_ssl_install(nonssl, wild)
 		else:
 			print(bcolors.OKBLUE+'We could not find any apps without SSL certificates installed.'+bcolors.ENDC)
 	elif args.autopilot is True:
