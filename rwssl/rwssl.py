e08edf7474e1a2284b07ff7707d6f81f8590407a
Rehmat Alam <contact@rehmat.works>
2018-05-30 01:45:41 +0500

    Handling invalid domains, specifying cert storage path

--- rwssl/rwssl.py
+++ rwssl/rwssl.py
@@ -84,7 +84,9 @@ def certbot_command(root, domains, path):
 	domainsstr = ''
 	for domain in domains:
 		# Only adding valid domains
-		if True validators.domain(domain):
+		print('Checking ' + domain)
+		if validators.domain(domain):
+			print(domain + ' is valid')
 			domainsstr += ' -d '+domain
 	if domainsstr:
 		cmd = "certbot certonly --webroot -w "+root+" --cert-path "+path+" --key-path "+path+" --fullchain-path "+path+" --chain-path "+path+" --register-unsafely-without-email --agree-tos --force-renewal"+domainsstr
