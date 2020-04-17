bd0a0d8ab262f458a59b2d081ba6ccd918bd3793
Rehmat Alam <contact@rehmat.works>
2020-04-18 04:04:09 +0500

    DNS validation

--- rwssl/utils.py
+++ rwssl/utils.py
@@ -180,7 +180,7 @@ class ServerPilot:
                 except:
                     ip = socket.gethostbyname(domain)
                     if validators.ipv4(ip) or validators.ipv6(ip):
-                        errmsg = 'SSL not available. {}\'s failing IP is {}'.format(ip)
+                        errmsg = 'A possible DNS issue found. {}\'s failing IP is {}'.format(ip)
                     else:
                         errmsg = 'SSL is not available for {} yet.'.format(domain)
                     print(colored(errmsg, 'yellow'))
