aa6f3011e7de025b76a130fb3bb644f643050c20
Rehmat Alam <contact@rehmat.works>
2020-04-18 05:29:16 +0500

    Err debug

--- rwssl/utils.py
+++ rwssl/utils.py
@@ -210,6 +210,7 @@ class ServerPilot:
                             domain)
                     print(colored(errmsg, 'yellow'))
         except Exception as e:
+            print(str(e))
             pass
 
         if len(validdoms) > 0:
