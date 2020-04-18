8a5f14c62ee21a855e8417248bff2da0155243fb
Rehmat Alam <contact@rehmat.works>
2020-04-18 17:22:19 +0500

    Using certbot full path in cron

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 import os
 import subprocess
 from setuptools.command.install import install
+import shutil
 
 class SetupSslRenewCron(install):
 	def run(self):
@@ -9,10 +10,9 @@ class SetupSslRenewCron(install):
 		cronfile = os.path.join(crondir, 'rwssl-sslrenewals')
 		if not os.path.exists(crondir):
 			os.makedirs(crondir)
-
-		certbotpath = subprocess.check_output(['which', 'certbot']).strip().decode('utf8')
+		cmd = '%s renew --non-interactive --config-dir /etc/nginx-sp/le-ssls --post-hook "service nginx-sp reload"\n' % shutil.which('certbot')
 		with open(cronfile, 'w') as cf:
-			cf.writelines(['#!/bin/sh\n', 'certbot renew --non-interactive --config-dir /etc/nginx-sp/le-ssls --post-hook "service nginx-sp reload"\n'])
+			cf.writelines(['#!/bin/sh\n', cmd])
 		maxexeccmd = "chmod +x {}".format(cronfile)
 		FNULL = open(os.devnull, 'w')
 		subprocess.check_call([maxexeccmd], shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
